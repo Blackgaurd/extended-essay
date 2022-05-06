7ac36f9d3f717e7d2aaa674e780c3beb6979a0ca
bryan <bryan.deng002@gmail.com>
2022-05-06 15:35:58 -0400

    change functions

--- edt/utils.py
+++ edt/utils.py
@@ -5,41 +5,17 @@
 import math
 
 
-def linear(depth: int, max_depth: int) -> float:
-    return 1 - depth / max_depth
+def linear(depth: int, optimal_depth: int) -> float:
+    return 1 - depth / optimal_depth
 
 
-def log10(depth: int, max_depth: int) -> float:
-    try:
-        return math.log10(-depth + max_depth + 1)
-    except ValueError as e:
-        print(depth, max_depth)
-        return float("-inf")
+def tanh(depth: int, optimal_depth: int) -> float:
+    return math.tanh(linear(depth, optimal_depth))
 
 
-def piecewise_linear(depth: int, max_depth: int) -> float:
-    return (
-        (max_depth - depth) / (2 * max_depth)
-        if depth < max_depth
-        else 2 * (max_depth - depth)
-    )
+def sigmoid(depth: int, optimal_depth: int) -> float:
+    return 1 / (1 + math.exp(depth - optimal_depth)) - 0.5
 
 
-def piecewise_negative_linear(depth: int, max_depth: int) -> float:
-    return min(0, linear(depth, max_depth))
-
-
-def piecewise_negative_log10(depth: int, max_depth: int) -> float:
-    return min(0, log10(depth, max_depth))
-
-
-def negative_quadratic(depth: int, max_depth: int) -> float:
-    return -(((depth - max_depth) / 2) ** 2)
-
-
-def piecewise_negative_quadratic(depth: int, max_depth: int) -> float:
-    return min(0, negative_quadratic(depth, max_depth))
-
-
-def no_f2(depth: int, max_depth: int) -> float:
-    return 0
+def f(depth: int, optimal_depth: int) -> float:
+    return min(0, linear(depth, optimal_depth))
